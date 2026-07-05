45928b0d43b6416890a72f99ab7b2c7b1f2def10
Eduardo Vera <Evera.21.xyz@gmail.com>
2026-07-05 10:26:58 -0500

    chore: agregar entorno de desarrollo con contenedores

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Instala Composer y NPM
+composer install
+npm install
+
+# Espera a la base de datos
+until nc -z db 3306; do
+  echo "Esperando a la base de datos..."
+  sleep 2
+done
+
+# Genera `APP_KEY`
+php artisan key:generate --force
+
+# Ejecuta la migración
+php artisan migrate --force
+php artisan optimize:clear
+
+# npm run build
+# php artisan serve --host=0.0.0.0 --port=3001
+exec apache2-foreground
